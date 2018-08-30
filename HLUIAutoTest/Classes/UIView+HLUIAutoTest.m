7d05ca3457049444c16a483bbb414d084fc2ec73
liuhualong <hualong8849@qq.com>
2018-08-30 14:53:16 +0800

    使用iosHL前缀来标识是否是由这个库生成的

--- HLUIAutoTest/Classes/UIView+HLUIAutoTest.m
+++ HLUIAutoTest/Classes/UIView+HLUIAutoTest.m
@@ -61,7 +61,7 @@
 - (NSString *)tb_accessibilityIdentifier
 {
     NSString *accessibilityIdentifier = [self tb_accessibilityIdentifier];
-    if (accessibilityIdentifier.length > 0 && [[accessibilityIdentifier substringToIndex:1] isEqualToString:@"("]) {
+    if (accessibilityIdentifier.length > 0 && [[accessibilityIdentifier substringToIndex:5] isEqualToString:@"iosHL"]) {
         return accessibilityIdentifier;
     }
     else if ([accessibilityIdentifier isEqualToString:@"null"]) {
@@ -71,23 +71,23 @@
     NSString *labelStr = [self.superview findNameWithInstance:self];
     
     if (labelStr && ![labelStr isEqualToString:@""]) {
-        labelStr = [NSString stringWithFormat:@"(%@)",labelStr];
+        labelStr = [NSString stringWithFormat:@"iosHL%@",labelStr];
     }
     else {
         if ([self isKindOfClass:[UILabel class]]) {//UILabel 使用 text
-            labelStr = [NSString stringWithFormat:@"(%@)",((UILabel *)self).text?:@""];
+            labelStr = [NSString stringWithFormat:@"iosHL%@",((UILabel *)self).text?:@""];
         }
         else if ([self isKindOfClass:[UIImageView class]]) {//UIImageView 使用 image 的 imageName
-            labelStr = [NSString stringWithFormat:@"(%@)",((UIImageView *)self).image.accessibilityIdentifier?:[NSString stringWithFormat:@"image%ld",(long)((UIImageView *)self).tag]];
+            labelStr = [NSString stringWithFormat:@"iosHL%@",((UIImageView *)self).image.accessibilityIdentifier?:[NSString stringWithFormat:@"image%ld",(long)((UIImageView *)self).tag]];
         }
         else if ([self isKindOfClass:[UIButton class]]) {//UIButton 使用 button 的 text 和 image
-            labelStr = [NSString stringWithFormat:@"(%@%@)",((UIButton *)self).titleLabel.text?:@"",((UIButton *)self).imageView.image.accessibilityIdentifier?:@""];
+            labelStr = [NSString stringWithFormat:@"iosHL%@%@",((UIButton *)self).titleLabel.text?:@"",((UIButton *)self).imageView.image.accessibilityIdentifier?:@""];
         }
         else if (accessibilityIdentifier) {// 已有 label，则在此基础上再次添加更多信息
-            labelStr = [NSString stringWithFormat:@"(%@)",accessibilityIdentifier];
+            labelStr = [NSString stringWithFormat:@"iosHL%@",accessibilityIdentifier];
         }
         if ([self isKindOfClass:[UIButton class]]) {
-            self.accessibilityValue = [NSString stringWithFormat:@"(%@)",((UIButton *)self).currentBackgroundImage.accessibilityIdentifier?:@""];
+            self.accessibilityValue = [NSString stringWithFormat:@"iosHL%@",((UIButton *)self).currentBackgroundImage.accessibilityIdentifier?:@""];
         }
     }
     if ([labelStr isEqualToString:@"()"] || [labelStr isEqualToString:@"(null)"] || [labelStr isEqualToString:@"null"]) {
@@ -102,14 +102,14 @@
     if ([self isKindOfClass:[UIImageView class]]) {//UIImageView 特殊处理
         NSString *name = [self.superview findNameWithInstance:self];
         if (name) {
-            self.accessibilityIdentifier = [NSString stringWithFormat:@"(%@)",name];
+            self.accessibilityIdentifier = [NSString stringWithFormat:@"iosHL%@",name];
         }
         else {
-            self.accessibilityIdentifier = [NSString stringWithFormat:@"(%@)",((UIImageView *)self).image.accessibilityIdentifier?:[NSString stringWithFormat:@"image%ld",(long)((UIImageView *)self).tag]];
+            self.accessibilityIdentifier = [NSString stringWithFormat:@"iosHL%@",((UIImageView *)self).image.accessibilityIdentifier?:[NSString stringWithFormat:@"image%ld",(long)((UIImageView *)self).tag]];
         }
     }
     if ([self isKindOfClass:[UITableViewCell class]]) {//UITableViewCell 特殊处理
-        self.accessibilityIdentifier = [NSString stringWithFormat:@"(%@)",((UITableViewCell *)self).reuseIdentifier];
+        self.accessibilityIdentifier = [NSString stringWithFormat:@"iosHL%@",((UITableViewCell *)self).reuseIdentifier];
     }
     return [self tb_accessibilityLabel];
 }
